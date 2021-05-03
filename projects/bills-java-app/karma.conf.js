e126ddb4644776118d44025dbcb536aaf2451351
Slawomir Kussowski <skussowski@advaoptical.com>
2021-05-03 10:12:20 +0200

    changed main app name

--- projects/bills-java-app/karma.conf.js
+++ projects/bills-java-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/bills-java'),
+      dir: require('path').join(__dirname, '../../coverage/bills-java-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- projects/bills-java-app/src/app/app.component.ts
+++ projects/bills-java-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bills-java';
+  title = 'bills-java-app';
 }
