bbda1bf16315d60266d083b8c18fb4c69969c813
Slawomir Kussowski <slawomirkussowski@gmail.com>
2021-05-08 17:31:26 +0200

    mock delays added

--- projects/bills-java-app/src/app/services/bills.service.ts
+++ projects/bills-java-app/src/app/services/bills.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Bill, BillsService } from 'projects/model/src/public-api';
 import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { PaymentsServiceImpl } from './payments.service';
 import { SchedulesServiceImpl } from './schedules.service';
 
@@ -17,13 +18,13 @@ export abstract class BillsServiceImpl extends BillsService {
 
   load(): Observable<Bill[]> { return of([]); }
 
-  fetchItem(id: number): Observable<Bill | null> { return of(null); }
+  fetchItem(id: number): Observable<Bill | null> { return of(null).pipe(delay(1000)); }
 
   getBills(): Bill[] { return []; }
 
-  fetchBills(): Observable<Bill[]> { return of([]); }
+  fetchBills(): Observable<Bill[]> { return of([]).pipe(delay(1000)); }
 
-  add(bill: Bill): Observable<number> { return of(0); }
+  add(bill: Bill): Observable<number> { return of(0).pipe(delay(1000)); }
 
   update(bill: Bill): Observable<void> { return of(); }
 
