620dbec88416643afefa5353bc7e0b042070326a
Slawomir Kussowski <skussowski@advaoptical.com>
2021-05-16 10:53:26 +0200

    added debounce time to app spinner to avoid loading state changes storm

--- projects/views/src/lib/components/root/root.component.ts
+++ projects/views/src/lib/components/root/root.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppSelectors, AppState } from 'projects/store/src/lib/state';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { AppSelectors, AppState } from 'projects/store/src/lib/state';
   styleUrls: ['./root.component.css']
 })
 export class RootComponent implements OnInit {
-  public loading$ = this.store.select(AppSelectors.selectLoading);
+  public loading$ = this.store.select(AppSelectors.selectLoading).pipe(debounceTime(250));
 
   constructor(private store: Store<AppState>) { }
 
