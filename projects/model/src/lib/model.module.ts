686ff615fda32c2574cb9a9060f7c7b30634bb23
Slawomir Kussowski <slawomirkussowskki@gmail.com>
2026-03-28 14:07:01 +0100

    removed redundant objects - continued

--- projects/model/src/lib/model.module.ts
+++ projects/model/src/lib/model.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
-import { ModelComponent } from './model.component';
 
 @NgModule({
-  declarations: [
-    ModelComponent
-  ],
   imports: [
-  ],
-  exports: [
-    ModelComponent
   ]
 })
 export class ModelModule { }
@@ -16,3 +9,4 @@ export class ModelModule { }
 export * from './helpers';
 export * from './model';
 export * from './services';
+
--- projects/model/src/public-api.ts
+++ projects/model/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of model
- */
-
-export * from './lib/model.service';
-export * from './lib/model.component';
 export * from './lib/model.module';
