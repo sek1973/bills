ceac213bd306b012b3679bf17a62d8892497b498
Slawomir Kussowski <slawomirkussowskki@gmail.com>
2026-04-12 08:17:47 +0200

    angular upgrade

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 13,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'indent': ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'windows'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      '@typescript-eslint/no-inferrable-types': [
+        'error',
+        { ignoreParameters: true, ignoreProperties: true },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
